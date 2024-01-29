08d724e96bd1d43804c5a5406abb054f94260730
Innaias <innaias@harveyosity.org>
2024-01-29 15:13:53 -0800

    Switched back to the normal Godot file dialog, after discovering how to use it properly

--- file_menu.gd
+++ file_menu.gd
@@ -2,4 +2,5 @@ extends PopupMenu
 
 func _ready() -> void:
 	set_item_accelerator(0, KEY_MASK_CTRL | KEY_O)
-	set_item_accelerator(1, KEY_MASK_CTRL | KEY_V)
+	set_item_accelerator(1, KEY_MASK_CTRL | KEY_C)
+	set_item_accelerator(2, KEY_MASK_CTRL | KEY_V)
--- main.gd
+++ main.gd
@@ -26,30 +26,23 @@ func _ready() -> void:
 			.replace(" ", "") # Remove all spaces between the types
 			.replace("*.", "") # Convert "*.png" into just "png"
 			.split(",")) # Split each type into an element
+	
+	for arg in OS.get_cmdline_args():
+		if _verify_path(arg):
+			set_image_from_file(arg)
+			break
 
 
 func _process(_delta: float) -> void:
-	image.custom_minimum_size.y = image_size * get_rect().size.y
+	image.custom_minimum_size = image_size * get_rect().size
 	$MarginContainer/BottomMenuRight/ZoomButton.text = str(roundf(image_size * 100)) + "%"
-	
-
-
-func _on_file_index_pressed(index: int) -> void:
-	match index:
-		0:
-			load_from_file()
-		1:
-			load_from_clipboard()
-		2:
-			$MarginContainer/TopMenu/TopMenu/File.set_item_checked(2, not $MarginContainer/TopMenu/TopMenu/File.is_item_checked(2))
-			get_window().always_on_top = $MarginContainer/TopMenu/TopMenu/File.is_item_checked(2)
+	if Input.is_action_just_released("ui_cancel"):
+		get_window().queue_free()
 
 
 func load_from_file() -> void:
 	$FileDialog.show()
-	var file: String = await $FileDialog.file_selected
-	if file:
-		set_image_from_file(file)
+	# Flow continues in _on_file_dialog_file_selected
 
 
 func set_image_from_file(file: String) -> void:
@@ -58,6 +51,7 @@ func set_image_from_file(file: String) -> void:
 	path_btn.text = file
 	var img = Image.new()
 	img.load(file)
+	img.generate_mipmaps()
 	var tex: ImageTexture = ImageTexture.create_from_image(img)
 	set_image(tex)
 
@@ -66,7 +60,9 @@ func load_from_clipboard() -> void:
 	$MarginContainer/BottomMenuLeft/PathButton.hide()
 	$MarginContainer/BottomMenuLeft/VSep.hide()
 	if DisplayServer.clipboard_has_image():
-		set_image(ImageTexture.create_from_image(DisplayServer.clipboard_get_image()))
+		var img: Image = DisplayServer.clipboard_get_image()
+		img.generate_mipmaps()
+		set_image(ImageTexture.create_from_image(img))
 		path_btn.text = path_str_from_clipboard
 
 
@@ -74,16 +70,37 @@ func set_image(img: ImageTexture) -> void:
 	$MarginContainer/BottomMenuLeft.show()
 	image.texture = img
 	image.material.set_shader_parameter("tex", img)
+	var img_size: Vector2 = img.get_size()
 	meta_btn.text = meta_str.format({
-			"w": img.get_width(),
-			"h": img.get_height(),
+			"w": img_size.x,
+			"h": img_size.y,
 			"f": format_to_str(img.get_format()),
 			})
+	
+	#image.expand_mode = TextureRect.EXPAND_FIT_WIDTH_PROPORTIONAL if img_size.x < img_size.y else TextureRect.EXPAND_FIT_HEIGHT_PROPORTIONAL
 
 
 func format_to_str(f: Image.Format) -> String:
 	var s: String = formats[f]
 	return s.split("_")[1]
+
+
+func _verify_path(path: String) -> bool:
+	var ext = path.split(".")[-1]
+	return ext in file_filter
+
+
+func _on_file_index_pressed(index: int) -> void:
+	match index:
+		0:
+			load_from_file()
+		1:
+			DisplayServer.clipboard_set(path_btn.text)
+		2:
+			load_from_clipboard()
+		3:
+			$MarginContainer/TopMenu/TopMenu/File.set_item_checked(2, not $MarginContainer/TopMenu/TopMenu/File.is_item_checked(2))
+			get_window().always_on_top = $MarginContainer/TopMenu/TopMenu/File.is_item_checked(2)
 
 
 func _on_texture_filter_options_item_selected(index: int) -> void:
@@ -109,8 +126,15 @@ func _on_clipboard_pressed(btn: Button) -> void:
 func _on_zoom_button_pressed() -> void:
 	image_size = 1.0
 
+
+
 func _on_window_files_dropped(files: PackedStringArray) -> void:
 	var file = files[0]
 	var ext = file.split(".")[-1]
 	if ext in file_filter:
 		set_image_from_file(files[0])
+
+
+func _on_file_dialog_file_selected(path: String) -> void:
+	if path:
+		set_image_from_file(path)
